793f546aa88d9b6fcb925f5cf63a1a4b67ce2a4c
srikant.kanchumurthi <srikant.kanchumurthi@REFL-RLP266.reflections.local>
2023-10-09 14:53:25 +0530

    commiting code

---
+++ Test Suites/Single Test Case.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Single Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>360182f1-b205-4ee8-b521-70842a8641bb</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4916c4d-1256-49f1-8165-67559ff910d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test7</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
